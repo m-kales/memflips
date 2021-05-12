aac779e97bb13b41d9f54d2843eedf92712f2f70
mikpearly <31659125+mikpearly@users.noreply.github.com>
2021-05-12 13:12:12 +0200

    Set up array of objects for cards

--- app.js
+++ app.js
@@ -2,7 +2,68 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const cardArray = [
         {
-            
-        }
+            name: 'beaver',
+            img: './assets/beaver.svg'
+        },
+        {
+            name: 'cheetah',
+            img: './assets/cheetah.svg'
+        },
+        {
+            name: 'chicken',
+            img: './assets/chicken.svg'
+        },
+        {
+            name: 'crocodile',
+            img: './assets/crocodile.svg'
+        },
+        {
+            name: 'dog',
+            img: './assets/dog.svg'
+        },
+        {
+            name: 'duck',
+            img: './assets/duck.svg'
+        },
+        {
+            name: 'horse',
+            img: './assets/horse.svg'
+        },
+        {
+            name: 'ladybug',
+            img: './assets/ladybug.svg'
+        },
+        {
+            name: 'lion',
+            img: './assets/lion.svg'
+        },
+        {
+            name: 'panda',
+            img: './assets/panda.svg'
+        },
+        {
+            name: 'parrot',
+            img: './assets/parrot.svg'
+        },
+        {
+            name: 'pig',
+            img: './assets/pig.svg'
+        },
+        {
+            name: 'rabbit',
+            img: './assets/rabbit.svg'
+        },
+        {
+            name: 'sheep',
+            img: './assets/sheep.svg'
+        },
+        {
+            name: 'snail',
+            img: './assets/snail.svg'
+        },
+        {
+            name: 'turtle',
+            img: './assets/turtle.svg'
+        },
     ]
 })
